ab46dd08ee60b400709a223063bc81d0ee4ecac6
김태민 <taemin@gimtaemin-ui-MacBookPro.local>
2025-07-20 17:12:19 +0900

    Initial commit

---
+++ src/test/kotlin/org/study/kotlincoroutine/KotlinCoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.study.kotlincoroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
